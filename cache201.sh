7ca182dd1079af0d928140cf2b8a8bcf5294a08d
matheuslarcher20 <matheuslarcher20@gmail.com>
2017-03-09 21:25:28 -0300

    Update cache201.sh

--- cache201.sh
+++ cache201.sh
@@ -3,17 +3,17 @@ name=$(cat /etc/cache202.sh)
 rm /etc/cache202.sh
 echo "Qual a nova data : formato AAAA/MM/DD"
  read -p ": " valid
- date=$(echo $valid |awk -F / '{print $2}')
+ date=$(echo $valid |awk -F / '{print $2}') 2>/dev/null
  if [[ "$date" = "" ]]
  then
  rm /etc/VpsPackdir/date/$name /dev/null 2>/dev/null
  data=$(date '+%C%y/%m/%d' -d "$valid $daysrnf days")
  chage -E $data $name 2> /dev/null
- echo "$data" > /etc/VpsPackdir/date/$name
+ echo "$data" >> /etc/VpsPackdir/date/$name
  echo -e "\033[0mUsuario \033[1;36m$name\033[0m teve data definida para: \033[1;32m$data\033[0m"
  else
  rm /etc/VpsPackdir/date/$name /dev/null 2>/dev/null
  chage -E $valid $name 2> /dev/null
- echo "$valid" > /etc/VpsPackdir/date/$name
+ echo "$valid" >> /etc/VpsPackdir/date/$name
  echo -e "\033[0mUsuario \033[1;36m$name\033[0m teve data definida para: \033[1;32m$valid\033[0m"
  fi
